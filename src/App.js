2f1a21fe180101876d3e76af731fe415adc44c96
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-07-06 15:53:30 -0600

    class based component search functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+      //set searchField as an empty string in state so we can use it throughout the component
+    };
+  }
+
+  componentDidMount() {
+    //code in here runs when component mounts/is placed on DOM
+    fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
+      response.json().then((data) =>
+        this.setState(
+          () => {
+            return { monsters: data };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      )
+    );
+  }
+
+  onSearchChange = (e) => {
+    console.log(e.target.value, 'e');
+    const searchField = e.target.value.toLowerCase();
+    //run toLowerCase to make the search not case sensitive
+    //calling this from above where we set in state
+    // better to move the method/function outside of render so the function is not called each time the component renders
+
+    this.setState(() => {
+      return { searchField };
+      //writing an object like this means that searchField is the name, and the value of it is the value. Key value pair
+      //setting the state to whatever is in the searchField?
+    });
+  };
+
+  render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+      //if the monsters name includes the search string, then return it
+      //otherwise it will not be returend
+      //filter give us back a new array
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          onChange={this.onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return <h1 key={monster.name}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
